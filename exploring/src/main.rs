088f9fd7ae8c52ee7c2559a679dff9e63ae69566
mluizaa00 <taeckls@gmail.com>
2021-04-21 18:57:21 -0300

    Updated main file on Exploring module

--- exploring/src/main.rs
+++ exploring/src/main.rs
@@ -1,9 +1,13 @@
-use rectangle::area;
-
 mod rectangle;
 mod packet;
 
 fn main() {
+    // Creates a new packet with the following information and prints them
     packet::create(String::from("Handshake"), 47, 32);
+
+    /*
+     * Creates a new Rectangle with the information provided and prints them
+     * plus the Area of the Rectangle
+    */
     rectangle::create(25.0, 10.0);
 }
